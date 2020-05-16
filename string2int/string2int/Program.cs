bc72093a5a0e3425767d57ba488675b86bd2480e
BeLL <narin456@hotmail.co.th>
2020-05-16 18:41:26 +0700

    Add files via upload

---
+++ string2int/string2int/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace string2int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string result = null;
+            Console.WriteLine("Enter Text To Int");
+            text = Console.ReadLine();
+            foreach (var x in text)
+            {
+                int myIntVal;
+                if(int.TryParse(x.ToString() , out myIntVal))
+                {
+                    result = result + myIntVal.ToString();
+                }
+            }
+            if (string.IsNullOrEmpty(result))
+                result = "null";
+            Console.Write("Result : " + result);
+            Console.ReadLine();
+        }
+    }
+}
